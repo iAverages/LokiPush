dd6beafc4304c028c496ba112f8de1831dda1a49
= <31937542+iAverages@users.noreply.github.com>
2022-06-19 21:40:29 +0100

    Add teamcity code coverage report

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,8 @@ const config = {
     testEnvironment: "node",
     testMatch: ["**/tests/**/*.?(m)js"],
     transform: {},
+    collectCoverage: true,
+    coverageReporters: ["lcov", "text", "teamcity"],
 };
 
 module.exports = config;
