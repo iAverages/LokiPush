2761bf4a131ac23a987e026b3dbe8b45d17c465e
= <31937542+iAverages@users.noreply.github.com>
2022-06-19 20:59:56 +0100

    Ensure loki corrected logged the logs

--- tests/integration/batcher.test.js
+++ tests/integration/batcher.test.js
@@ -1,6 +1,11 @@
-const { default: Batcher } = require("../../dist");
+const axios = require("axios").default;
+const Batcher = require("../../dist").default;
 const url = "http://loki:3100/loki/api/v1/push";
-const logs = ["loki test", "just another loki test", "loki loki loki!", { key: "value" }];
+const fetchUrl = "http://loki:3100/loki/api/v1/query?query=%7Bsource%3D%22lokijs%22%7D&direction=forward";
+// TODO: Add test for logging objects - { key: "value" }
+const logs = ["loki test", "just another loki test", "loki loki loki!"];
+
+jest.setTimeout(10000);
 
 describe("Logs", () => {
     it("should log to loki", async () => {
@@ -9,5 +14,16 @@ describe("Logs", () => {
             loki.addLog(log);
         }
         await loki.push();
+
+        const res = await axios.get(fetchUrl);
+        if (res.data.status !== "success" && res.data.data.result[0].values < 1) {
+            fail();
+        }
+        const lokiLogs = res.data.data.result[0].values;
+        for (let i = 0; i < lokiLogs.length; i++) {
+            const log = logs[i];
+            const lokiLog = lokiLogs[i][1];
+            expect(log).toBe(lokiLog);
+        }
     });
 });
