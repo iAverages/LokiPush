3d10171699dda1fa547ab04d490490fd4b3c1785
= <31937542+iAverages@users.noreply.github.com>
2022-06-19 14:45:30 +0100

    Prepare for tests

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+// Sync object
+/** @type {import('@jest/types').Config.InitialOptions} */
+const config = {
+    testEnvironment: "node",
+    testMatch: ["**/tests/**/*.?(m)js"],
+    transform: {},
+};
+
+module.exports = config;
